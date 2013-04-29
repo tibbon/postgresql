e0a5b7da6833a868502b41daf4f97660b3c2564e
David Fisher <tibbon+github@gmail.com>
2013-04-28 22:36:24 -0300

    Update server.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -62,7 +62,7 @@ template "#{node['postgresql']['dir']}/postgresql.conf" do
   owner "postgres"
   group "postgres"
   mode 0600
-  notifies :restart, 'service[postgresql]', :immediately
+  notifies :reload, 'service[postgresql]', :immediately
 end
 
 template "#{node['postgresql']['dir']}/pg_hba.conf" do
@@ -70,7 +70,7 @@ template "#{node['postgresql']['dir']}/pg_hba.conf" do
   owner "postgres"
   group "postgres"
   mode 00600
-  notifies :restart, 'service[postgresql]', :immediately
+  notifies :reload, 'service[postgresql]', :immediately
   #notifies :reload, 'service[postgresql]', :immediately
 end
 
